b93c06390aaf99d400ab3f7e7804339c31dac8b4
Midengrows1 <midengrows@outlook.com>
2023-12-26 02:32:47 +0500

    added backend

--- Backend/AuthRouter.js
+++ Backend/AuthRouter.js
@@ -6,7 +6,7 @@ import {
   verifyToken,
   createMemory,
   upload,
-<<<<<<< HEAD
+
   getMemories,
   getUser,
   deleteMemory,
@@ -15,8 +15,6 @@ import {
   forgetPassword,
   resetPasswordFunc,
   resetPassword,
-=======
->>>>>>> 710c6c4426870ba8087e26f1e58be4f9b66177fc
 } from "./AuthControler.js";
 
 const router = new Router();
@@ -24,7 +22,6 @@ const router = new Router();
 router.post("/registration", registration);
 router.post("/login", login);
 router.get("/protected", verifyToken);
-<<<<<<< HEAD
 router.post("/create", verifyToken, upload.single("image"), createMemory);
 router.get("/", verifyToken, getMemories);
 router.get("/api/userInfo", verifyToken, getUser);
@@ -34,7 +31,7 @@ router.get("/memories", verifyToken, searchMemories);
 router.post("/forgot-password", forgetPassword);
 router.get("/reset-password/:token", resetPasswordFunc);
 router.post("/reset-password", resetPassword);
-=======
+
 router.post("/create", upload.single("image"), createMemory);
->>>>>>> 710c6c4426870ba8087e26f1e58be4f9b66177fc
+
 export default router;
