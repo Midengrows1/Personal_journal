d2f1ca857b98ee36dff90b4c20e88e3186035b9e
Midengrows1 <midengrows@outlook.com>
2023-12-26 15:54:38 +0500

    changed cors

--- Backend/index.js
+++ Backend/index.js
@@ -23,7 +23,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
