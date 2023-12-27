e67c171fb0b9c2fb53f0a500f75f134ff0841a2c
Midengrows1 <midengrows@outlook.com>
2023-12-27 14:38:49 +0500

    route change

--- Backend/AuthRouter.js
+++ Backend/AuthRouter.js
@@ -24,7 +24,7 @@ router.get("/protected", verifyToken);
 router.post("/create", verifyToken, upload.single("image"), createMemory);
 router.get("/home", verifyToken, getMemories);
 router.get("/", (req, res) => {
-  res.json({ message: "public hello" });
+  res.send("public hello");
 });
 router.get("/api/userInfo", verifyToken, getUser);
 router.delete("/delete/:memoryId", verifyToken, deleteMemory);
